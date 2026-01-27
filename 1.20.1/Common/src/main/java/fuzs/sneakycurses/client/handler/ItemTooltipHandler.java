4b04d8c7c3addc348290dce224b9ccb46ccd1297
Danieltl21 <danieltrevisan532@gmail.com>
2026-01-27 20:21:23 -0300

    Update ItemTooltipHandler.java
    Fallback mode for curses

--- 1.20.1/Common/src/main/java/fuzs/sneakycurses/client/handler/ItemTooltipHandler.java
+++ 1.20.1/Common/src/main/java/fuzs/sneakycurses/client/handler/ItemTooltipHandler.java
@@ -46,7 +46,8 @@ public class ItemTooltipHandler {
         if (!isAffected(player, itemStack)) return;
         ListIterator<Component> iterator = lines.listIterator();
         while (iterator.hasNext()) {
-            if (iterator.next().getContents() instanceof TranslatableContents contents && contents.getKey().startsWith("enchantment.")) {
+            Component componentLine = iterator.next();
+            if (componentLine.getContents() instanceof TranslatableContents contents && contents.getKey().startsWith("enchantment.")) {
                 String[] enchantmentKey = contents.getKey().split("\\.");
                 Enchantment enchantment = null;
                 if (enchantmentKey.length >= 3) {
@@ -65,6 +66,14 @@ public class ItemTooltipHandler {
                         iterator.remove();
                     }
                 }
+            }
+            // fallback mode for non-translatable curses
+            else if (SneakyCurses.CONFIG.get(ServerConfig.class).fallbackMode &&
+                        componentLine.getString().startsWith("Curse of"))
+            {
+                initSeed(currentScreenSeed + componentLine.getString().substring(9).hashCode());
+                Component component = getLoreForWidth(Minecraft.getInstance().font);
+                iterator.set(Component.empty().append(component).withStyle(ChatFormatting.RED));
             }
         }
     }
